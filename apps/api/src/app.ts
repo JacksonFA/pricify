037c549b5b719e92de8860e48bee9e944d51dc89
JacksonFA <jackson.f205@gmail.com>
2024-09-10 16:13:51 -0300

    feat(project): start a yarn workspace and add initial api project

---
+++ apps/api/src/app.ts
@@ -0,0 +1,34 @@
+import os from 'node:os'
+import { env } from '@/env'
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
+})
+app.register(fastifyCookie)
+app.get('/', () => ({ message: `Tagit API running on ${os.hostname()}` }))
+// app.register(ROUTES)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
